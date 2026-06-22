70cc61e31baae190b7fa15fca79c0e61f89e4747
thedev-abhishek <thedev.abhishek@gmail.com>
2026-06-22 20:10:12 +0530

    Added FB

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
   const currentYear = new Date().getFullYear();
 
   const socialLinks = [
-    { icon: Facebook, href: '#', color: 'hover:bg-blue-600', label: 'Facebook' },
+    { icon: Facebook, href: 'https://www.facebook.com/profile.php?id=61591331787244', color: 'hover:bg-blue-600', label: 'Facebook' },
     { icon: Twitter, href: 'https://x.com/MovinVAInc', color: 'hover:bg-blue-400', label: 'Twitter' },
     { icon: Linkedin, href: 'https://www.linkedin.com/company/movin-va/', color: 'hover:bg-blue-700', label: 'LinkedIn' },
     { icon: Instagram, href: 'https://www.instagram.com/movin_va/', color: 'hover:bg-pink-600', label: 'Instagram' },
